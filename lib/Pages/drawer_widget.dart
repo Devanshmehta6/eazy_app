f22205b88d6b8033d789563216a633e38fdb217b
Devanshmehta6 <mehta.devansh0603@gmail.com>
2021-10-07 23:12:29 +0530

    commit

--- lib/Pages/drawer_widget.dart
+++ lib/Pages/drawer_widget.dart
@@ -1,7 +1,9 @@
+
 import 'dart:io';
 import 'dart:convert';
 import 'package:eazy_app/Pages/customer_check.dart/first.dart';
 import 'package:eazy_app/Services/auth_service.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:eazy_app/Pages/dashboard.dart';
 import 'package:eazy_app/Pages/eazy_teams.dart';
@@ -12,6 +14,7 @@ import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_session/flutter_session.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:configurable_expansion_tile/configurable_expansion_tile.dart';
 
 // import 'package:dio/dio.dart' as http;
 import 'package:eazy_app/Services/teams_json.dart';
@@ -25,6 +28,21 @@ class _NavigationDrawerWidgetState extends State<NavigationDrawerWidget> {
   final padding = EdgeInsets.symmetric(horizontal: 10);
   Color myColor = Color(0xff4044fc);
   List<User> users = [];
+  bool pressed = false;
+
+  // changeText() {
+  //   if (pressed = true) {
+  //     return Text(
+  //       'True',
+  //       style: TextStyle(color: Colors.red),
+  //     );
+  //   } else {
+  //     return Text(
+  //       'False',
+  //       style: TextStyle(color: Colors.green),
+  //     );
+  //   }
+  // }
 
   Future<List<User>> getUsers() async {
     final pref = await SharedPreferences.getInstance();
@@ -74,11 +92,13 @@ class _NavigationDrawerWidgetState extends State<NavigationDrawerWidget> {
         MediaQuery.of(context).padding.top -
         kToolbarHeight;
     final width = MediaQuery.of(context).size.width;
+
     return Drawer(
       child: Column(
         children: <Widget>[
           Container(
-            height: 165,
+            height: height * 0.2,
+            padding: EdgeInsets.only(top: height * 0.01),
             child: DrawerHeader(
               padding:
                   EdgeInsets.only(left: 10, right: 10, bottom: 15, top: 15),
@@ -87,15 +107,128 @@ class _NavigationDrawerWidgetState extends State<NavigationDrawerWidget> {
               ),
             ),
           ),
-          ListTile(
-            title: Text(
-              'Eazy Dashboard',
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  fontSize: 18,
-                  color: myColor,
-                ),
+          //Divider(),
+         // changeText(),
+          // ListTile(
+          //   title: Row(
+          //     children: [
+          //       Icon(FontAwesomeIcons.chartPie, color: myColor),
+          //       SizedBox(width: width * 0.06),
+          //       Text('EazyDashboard'),
+          //     ],
+          //   ),
+          //   onTap: () {
+          //     setState(() {
+          //       pressed = false;
+          //     });
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(
+          //         builder: (context) => Dashboard(),
+          //       ),
+          //     );
+          //   },
+          // ),
+          SizedBox(height: height * 0.02),
+          Theme(
+            data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+            child: ExpansionTile(
+              title: Row(
+                children: [
+                  Icon(FontAwesomeIcons.users),
+                  SizedBox(width: width * 0.06),
+                  Text(
+                    'EazyVisits',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                        color: myColor,
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              children: [
+                Container(
+                  child: FutureBuilder(
+                    future: getUsers(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null) {
+                        return CircularProgressIndicator();
+                      } else {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              child: Theme(
+                                data: Theme.of(context)
+                                    .copyWith(dividerColor: Colors.transparent),
+                                child: Row(
+                                  children: [
+                                    SizedBox(width: width * 0.08),
+                                    Icon(FontAwesomeIcons.userClock),
+                                    SizedBox(width: width * 0.05),
+                                    Container(
+                                      padding: EdgeInsets.only(top: 10),
+                                      child: Text(
+                                        snapshot.data[index].project_name,
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 16, color: myColor),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: height * 0.02),
+          Theme(
+            data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+            child: ExpansionTile(
+              title: Row(
+                children: [
+                  Icon(FontAwesomeIcons.userTie),
+                  SizedBox(width: width * 0.06),
+                  Text(
+                    'EazyTeams',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                        color: myColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              children: [],
+            ),
+          ),
+          SizedBox(height: height * 0.02),
+          Divider(thickness: 0.3),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(FontAwesomeIcons.signOutAlt),
+                SizedBox(width: width * 0.06),
+                Text(
+                  'Log Out',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      color: myColor,
+                    ),
+                  ),
+                ),
+              ],
             ),
             onTap: () {
               Navigator.push(
@@ -106,267 +239,9 @@ class _NavigationDrawerWidgetState extends State<NavigationDrawerWidget> {
               );
             },
           ),
-          ExpansionTile(
-            
-            title: Text(
-              'Eazy Visits',
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  fontSize: 18,
-                  color: myColor,
-                ),
-              ),
-            ),
-            children: [
-              Container(
-                child: FutureBuilder(
-                  future: getUsers(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return CircularProgressIndicator();
-                    } else {
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            
-                            contentPadding: EdgeInsets.all(0),
-                            dense: true,
-                            visualDensity: VisualDensity(vertical: -4),
-                            title: Row(
-                              children: [
-                                SizedBox(height: height * 0.05),
-                                Padding(
-                                  padding: EdgeInsets.only(left: width * 0.18),
-                                  child: Image.asset('images/bullet.jpg',
-                                      height: 13),
-                                ),
-                                SizedBox(width: width * 0.05),
-                                Text(
-                                  snapshot.data[index].project_name,
-                                  style: GoogleFonts.poppins(
-                                    textStyle: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
         ],
       ),
     );
-  }
-}
-
-class Project extends StatefulWidget {
-  @override
-  State<Project> createState() => _ProjectState();
-}
-
-class _ProjectState extends State<Project> {
-  Map mapResponse = {};
-  List<User> users = [];
-
-  late String token;
-  late String settoken;
-
-  Future<List<User>> getUsers() async {
-    final pref = await SharedPreferences.getInstance();
-
-    final isLoggedIn = pref.getBool('log');
-    print('Logged in dashboard : $isLoggedIn');
-
-    if (isLoggedIn == true) {
-      Uri url = Uri.parse('https://geteazyapp.com/dashboard_api/');
-
-      String sessionId = await FlutterSession().get('session');
-
-      String csrf = await FlutterSession().get('csrf');
-
-      final sp = await SharedPreferences.getInstance();
-      String? authorization = sp.getString('token');
-      final token = await AuthService.getToken();
-      String? tokenn = authorization;
-      final cookie = sp.getString('cookie');
-      final settoken = 'Token ${token['token']}';
-      print('Set token :: $settoken');
-
-      final setcookie = "csrftoken=$csrf; sessionid=$sessionId";
-      http.Response response = await http.get(url, headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': settoken,
-        HttpHeaders.cookieHeader: setcookie,
-      });
-      final jsonData = jsonDecode(response.body);
-      final projectData = jsonData['projects'];
-
-      for (var u in projectData) {
-        User user = User(u["project_name"], u["project_url"]);
-        users.add(user);
-      }
-      //return users;
-    } else {
-      print('Logged Out...');
-    }
-    return users;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
-
-    final color = Color(0xff4044fc);
-    final fontFamily = 'Poppins';
-    final fontSize = 16.0;
-    final fontWeight = FontWeight.w500;
-    return Container(
-        child: Theme(
-      data: theme,
-      child: Column(children: <Widget>[
-        ExpansionTile(
-          title: Text(
-            'EazyVisits',
-            style: TextStyle(
-              color: color,
-              fontFamily: fontFamily,
-              fontSize: fontSize,
-            ),
-          ),
-          onExpansionChanged: (e) {},
-          children: [
-            Expanded(
-              child: SizedBox(
-                height: 200,
-                child: FutureBuilder(
-                  future: getUsers(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return Container(
-                        child: Center(
-                          child: Text('Loading..'),
-                        ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        controller: ScrollController(),
-                        scrollDirection: Axis.vertical,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            title: Text(
-                                'Name  : ${snapshot.data[index].project_name}'),
-                          );
-                        },
-                      );
-                    }
-                  },
-                ),
-              ),
-            ),
-            // OutlinedButton(
-            //     child: Text('uibgfu[qb'),
-            //     onPressed: () {
-            //       Navigator.push(context,
-            //           MaterialPageRoute(builder: (context) => EazyVisits()));
-            //     }),
-          ],
-        ),
-      ]),
-    ));
-  }
-}
-
-class ProjectTeams extends StatefulWidget {
-  @override
-  State<ProjectTeams> createState() => _ProjectTeamsState();
-}
-
-class _ProjectTeamsState extends State<ProjectTeams> {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
-    final color = Color(0xff4044fc);
-    final fontFamily = 'Poppins';
-    //final fontSize = 16.0;
-    final fontWeight = FontWeight.w500;
-    return Container(
-      child: Theme(
-        data: theme,
-        child: Column(children: <Widget>[
-          ExpansionTile(
-            title: Text(
-              'EazyTeams',
-              style: TextStyle(
-                color: color,
-                fontFamily: fontFamily,
-                fontSize: 16,
-              ),
-            ),
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 50),
-                child: Row(
-                  children: <Widget>[
-                    Image.asset('images/bullet.jpg', height: 15, width: 15),
-                    OutlinedButton(
-                      onPressed: () {
-                        //sabKuch();
-
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => EazyTeams(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'UrbanPlace Project',
-                        style: TextStyle(
-                            color: color,
-                            fontSize: 16,
-                            fontFamily: fontFamily,
-                            fontWeight: fontWeight),
-                      ),
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: Colors.white, width: 0),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 3.7),
-              )
-            ],
-          ),
-        ]),
-      ),
-    );
-  }
-}
-
-void selectedItem(BuildContext context, int index) {
-  switch (index) {
-    case 0:
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => Dashboard(),
-        ),
-      );
-      break;
   }
 }
 
